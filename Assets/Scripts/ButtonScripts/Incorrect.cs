6a984af00745750105ae018f2d37ad292230e2ab
triAngleOverlord <2328564@students.wits.ac.za>
2023-08-20 15:11:37 +0200

    fixing 1

--- Assets/Scripts/ButtonScripts/Incorrect.cs
+++ Assets/Scripts/ButtonScripts/Incorrect.cs
@@ -1,16 +1,25 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class Incorrect : MonoBehaviour
 {
     public void Awake()
     {
+        var text= GameObject.Find("GameManager").transform.GetComponent<GameManager>().sequence;
+        var txtx = GameObject.Find("GameManager").transform.GetComponent<GameManager>().submission;
+        for (int i = 0; i < text.Count; i++)
+        {
+            GetComponent<TextMeshProUGUI>().text += text[i] + " and submitted " + txtx[i];
+        }
+
+        
         StartCoroutine(incorrectLiveTime());
     }
 
     public IEnumerator incorrectLiveTime()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(10);
         Destroy(gameObject);
     }
 }
